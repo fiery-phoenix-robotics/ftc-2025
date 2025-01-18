1c51d3af0b9cdc85bd7549109e8ba9c69d931a13
scientistofeverything <noah.e.haskins@gmail.com>
2025-01-18 17:21:31 -0500

    Moved old files to legacy folder

--- reorganized/OpModes/TeleOp/FirstTeleOp.java
+++ reorganized/OpModes/TeleOp/FirstTeleOp.java
@@ -11,7 +11,7 @@ import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 public class FirstTeleOp extends LinearOpMode {
     
     public Drivetrain drivetrain = new Drivetrain();
-
+    
     @Override
     public void runOpMode () {
 
@@ -19,7 +19,7 @@ public class FirstTeleOp extends LinearOpMode {
         drivetrain.rightDriveFront = hardwareMap.get(DcMotor.class, "rightDriveFront");
         drivetrain.leftDriveRear  = hardwareMap.get(DcMotor.class, "leftDriveRear");
         drivetrain.rightDriveRear = hardwareMap.get(DcMotor.class, "rightDriveRear");
-        
+
         drivetrain.init();
 
         waitForStart();
