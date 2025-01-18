01aede694f1b4e11ea298207c0eb24e4f8770517
scientistofeverything <noah.e.haskins@gmail.com>
2025-01-18 14:19:17 -0500

    Update Constants.java

--- reorganized/Constants.java
+++ reorganized/Constants.java
@@ -4,7 +4,8 @@ public final class Constants {
     public final class Drivetrain {
         public static double power = 1.0;
     }
-    public init () {
-        // configure otos
+    public static final class Arm {
+        public static final int ARM_MOTOR_POS_INIT = 0;
+        public static final int WRIST_MOTOR_POS_INIT = 0;
     }
 }
